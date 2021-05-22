0053137b7573065500599785a7170aa7aa337dec
MelanX <info@melanx.de>
2021-05-22 15:05:42 +0200

    add code for upcoming event
    waiting for https://github.com/MinecraftForge/MinecraftForge/pull/7715 to get merged

--- src/main/java/de/melanx/utilitix/EventListener.java
+++ src/main/java/de/melanx/utilitix/EventListener.java
@@ -1,29 +1,20 @@
 package de.melanx.utilitix;
 
-import de.melanx.utilitix.item.Quiver;
 import de.melanx.utilitix.item.bells.MobBell;
 import de.melanx.utilitix.registration.ModItems;
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.IFormattableTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.World;
-import net.minecraftforge.event.entity.player.ArrowNockEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.items.IItemHandlerModifiable;
-
-import java.util.stream.Stream;
 
 public class EventListener {
 
@@ -55,31 +46,38 @@ public class EventListener {
         }
     }
 
-    @SubscribeEvent
-    public void onBowMimimi(ArrowNockEvent event) {
-        World world = event.getWorld();
-
-        if (!event.hasAmmo()) {
-            PlayerEntity player = event.getPlayer();
-            Stream.concat(Stream.of(player.getHeldItemOffhand()), player.inventory.mainInventory.stream())
-                    .filter(stack -> stack.getItem() == ModItems.quiver)
-                    .filter(stack -> !Quiver.isEmpty(stack))
-                    .findFirst()
-                    .ifPresent(stack -> {
-                        IItemHandlerModifiable inventory = Quiver.getInventory(stack);
-                        assert inventory != null;
-                        int enchantmentLevel = EnchantmentHelper.getEnchantmentLevel(Enchantments.INFINITY, stack);
-                        if (enchantmentLevel >= 1) {
-                            event.setAction(ActionResult.resultConsume(event.getBow()));
-                        } else {
-                            for (int i = 0; i < inventory.getSlots(); i++) {
-                                if (inventory.extractItem(i, 1, player.isCreative()).isEmpty()) {
-                                    event.setAction(ActionResult.resultConsume(event.getBow()));
-                                    return;
-                                }
-                            }
-                        }
-                    });
-        }
-    }
+    // TODO wait for https://github.com/MinecraftForge/MinecraftForge/pull/7715
+//    @SubscribeEvent
+//    public void onBowFindAmmo(PlayerFindProjectileEvent event) {
+//        if (event.getFoundAmmo().isEmpty()) {
+//            PlayerEntity player = event.getPlayer();
+//            Stream.concat(Stream.of(player.getHeldItemOffhand()), player.inventory.mainInventory.stream())
+//                    .filter(stack -> stack.getItem() == ModItems.quiver)
+//                    .filter(stack -> !Quiver.isEmpty(stack))
+//                    .findFirst()
+//                    .ifPresent(stack -> {
+//                        IItemHandlerModifiable inventory = Quiver.getInventory(stack);
+//                        assert inventory != null;
+//                        int enchantmentLevel = EnchantmentHelper.getEnchantmentLevel(Enchantments.INFINITY, stack);
+//                        if (enchantmentLevel >= 1) {
+//                            for (int i = 0; i < inventory.getSlots(); i++) {
+//                                ItemStack arrow = inventory.getStackInSlot(i);
+//                                if (!arrow.isEmpty()) {
+//                                    event.setAmmo(arrow.copy());
+//                                    return;
+//                                }
+//                            }
+//                        } else {
+//                            for (int i = 0; i < inventory.getSlots(); i++) {
+//                                ItemStack arrow = inventory.getStackInSlot(i);
+//                                if (!arrow.isEmpty()) {
+//                                    arrow = player.isCreative() ? arrow.copy() : arrow;
+//                                    event.setAmmo(arrow);
+//                                    return;
+//                                }
+//                            }
+//                        }
+//                    });
+//        }
+//    }
 }
