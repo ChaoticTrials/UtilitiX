22730a689d6af364a3f532e02e83dcc57d06a829
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-05-25 17:11:28 +0200

    Fix weird glue behaviour

--- src/main/java/de/melanx/utilitix/item/ItemGlueBall.java
+++ src/main/java/de/melanx/utilitix/item/ItemGlueBall.java
@@ -19,26 +19,23 @@ public class ItemGlueBall extends ItemBase {
     @Nonnull
     @Override
     public ActionResultType onItemUse(@Nonnull ItemUseContext context) {
-        if (!context.getWorld().isRemote) {
-            Chunk chunk = context.getWorld().getChunkAt(context.getPos());
-            //noinspection ConstantConditions
-            StickyChunk glue = chunk.getCapability(SlimyCapability.STICKY_CHUNK).orElse(null);
-            //noinspection ConstantConditions
-            if (glue != null) {
-                int x = context.getPos().getX() & 0xF;
-                int y = context.getPos().getY();
-                int z = context.getPos().getZ() & 0xF;
-                if (!glue.get(x, y, z, context.getFace()) && SlimyCapability.canGlue(context.getWorld(), context.getPos(), context.getFace())) {
+        Chunk chunk = context.getWorld().getChunkAt(context.getPos());
+        //noinspection ConstantConditions
+        StickyChunk glue = chunk.getCapability(SlimyCapability.STICKY_CHUNK).orElse(null);
+        //noinspection ConstantConditions
+        if (glue != null) {
+            int x = context.getPos().getX() & 0xF;
+            int y = context.getPos().getY();
+            int z = context.getPos().getZ() & 0xF;
+            if (!glue.get(x, y, z, context.getFace()) && SlimyCapability.canGlue(context.getWorld(), context.getPos(), context.getFace())) {
+                if (!context.getWorld().isRemote) {
                     glue.set(x, y, z, context.getFace(), true);
                     chunk.markDirty();
                     if (context.getPlayer() == null || !context.getPlayer().abilities.isCreativeMode) {
                         context.getItem().shrink(1);
                     }
-                    if (context.getPlayer() != null) {
-                        context.getPlayer().swing(context.getHand(), true);
-                    }
-                    return ActionResultType.CONSUME;
                 }
+                return ActionResultType.successOrConsume(context.getWorld().isRemote);
             }
         }
         return super.onItemUse(context);
