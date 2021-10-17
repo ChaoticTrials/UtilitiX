39edf67385ac3c3f069a435d8549c59b6584488d
MelanX <info@melanx.de>
2021-10-17 17:51:10 +0200

    add jade support for experience crystal

---
+++ src/main/java/de/melanx/utilitix/compat/jade/ExperienceCrystalProvider.java
@@ -0,0 +1,50 @@
+package de.melanx.utilitix.compat.jade;
+
+import de.melanx.utilitix.content.experiencecrystal.TileExperienceCrystal;
+import de.melanx.utilitix.util.XPUtils;
+import mcp.mobius.waila.api.BlockAccessor;
+import mcp.mobius.waila.api.IComponentProvider;
+import mcp.mobius.waila.api.IServerDataProvider;
+import mcp.mobius.waila.api.ITooltip;
+import mcp.mobius.waila.api.config.IPluginConfig;
+import mcp.mobius.waila.api.ui.IElementHelper;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import snownee.jade.Jade;
+
+public class ExperienceCrystalProvider implements IComponentProvider, IServerDataProvider<BlockEntity> {
+
+    public static final ExperienceCrystalProvider INSTANCE = new ExperienceCrystalProvider();
+    private static final ItemStack XP_BOTTLE = new ItemStack(Items.EXPERIENCE_BOTTLE);
+
+    @Override
+    public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
+        if (!config.get(UtilJade.EXPERIENCE_CRYSTAL)) {
+            return;
+        }
+
+        int xp = accessor.getServerData().getInt("Xp");
+        IElementHelper helper = tooltip.getElementHelper();
+        tooltip.add(helper.item(XP_BOTTLE, 0.75f));
+        if (accessor.getServerData().getBoolean("ShowDetails")) {
+            tooltip.append(helper.text(new TranslatableComponent("jade.utilitix.experience_crystal.xp")).translate(Jade.VERTICAL_OFFSET));
+            tooltip.append(helper.text(new TextComponent(String.valueOf(xp))).translate(Jade.VERTICAL_OFFSET));
+        } else {
+            tooltip.append(helper.text(new TranslatableComponent("jade.utilitix.experience_crystal.level")).translate(Jade.VERTICAL_OFFSET));
+            tooltip.append(helper.text(new TextComponent(XPUtils.getLevelExp(xp).getLeft().toString())).translate(Jade.VERTICAL_OFFSET));
+        }
+    }
+
+    @Override
+    public void appendServerData(CompoundTag data, ServerPlayer player, Level level, BlockEntity blockEntity, boolean showDetails) {
+        TileExperienceCrystal crystal = (TileExperienceCrystal) blockEntity;
+        data.putInt("Xp", crystal.getXp());
+        data.putBoolean("ShowDetails", showDetails);
+    }
+}
--- src/main/java/de/melanx/utilitix/compat/jade/UtilJade.java
+++ src/main/java/de/melanx/utilitix/compat/jade/UtilJade.java
@@ -5,6 +5,8 @@ import de.melanx.utilitix.content.brewery.BlockAdvancedBrewery;
 import de.melanx.utilitix.content.brewery.TileAdvancedBrewery;
 import de.melanx.utilitix.content.crudefurnace.BlockCrudeFurnace;
 import de.melanx.utilitix.content.crudefurnace.TileCrudeFurnace;
+import de.melanx.utilitix.content.experiencecrystal.BlockExperienceCrystal;
+import de.melanx.utilitix.content.experiencecrystal.TileExperienceCrystal;
 import de.melanx.utilitix.content.wireless.BlockLinkedRepeater;
 import de.melanx.utilitix.content.wireless.TileLinkedRepeater;
 import mcp.mobius.waila.api.IRegistrar;
@@ -16,22 +18,27 @@ import net.minecraft.resources.ResourceLocation;
 @WailaPlugin
 public class UtilJade implements IWailaPlugin {
 
-    public static final ResourceLocation CRUDE_FURNACE = UtilitiX.getInstance().resource("crude_furnace");
-    public static final ResourceLocation LINKED_REPEATER = UtilitiX.getInstance().resource("linked_repeater");
     public static final ResourceLocation ADVANCED_BREWERY = UtilitiX.getInstance().resource("advanced_brewery");
+    public static final ResourceLocation CRUDE_FURNACE = UtilitiX.getInstance().resource("crude_furnace");
+    public static final ResourceLocation EXPERIENCE_CRYSTAL = UtilitiX.getInstance().resource("experience_crystal");
+    public static final ResourceLocation LINKED_REPEATER = UtilitiX.getInstance().resource("linked_repeater");
 
     @Override
     public void register(IRegistrar registrar) {
+        registrar.registerComponentProvider(AdvancedBreweryProvider.INSTANCE, TooltipPosition.BODY, BlockAdvancedBrewery.class);
+        registrar.registerBlockDataProvider(AdvancedBreweryProvider.INSTANCE, TileAdvancedBrewery.class);
+        registrar.addConfig(ADVANCED_BREWERY, true);
+
         registrar.registerComponentProvider(CrudeFurnaceProvider.INSTANCE, TooltipPosition.BODY, BlockCrudeFurnace.class);
         registrar.registerBlockDataProvider(CrudeFurnaceProvider.INSTANCE, TileCrudeFurnace.class);
         registrar.addConfig(CRUDE_FURNACE, true);
 
+        registrar.registerComponentProvider(ExperienceCrystalProvider.INSTANCE, TooltipPosition.BODY, BlockExperienceCrystal.class);
+        registrar.registerBlockDataProvider(ExperienceCrystalProvider.INSTANCE, TileExperienceCrystal.class);
+        registrar.addConfig(EXPERIENCE_CRYSTAL, true);
+
         registrar.registerComponentProvider(LinkedRepeaterProvider.INSTANCE, TooltipPosition.BODY, BlockLinkedRepeater.class);
         registrar.registerBlockDataProvider(LinkedRepeaterProvider.INSTANCE, TileLinkedRepeater.class);
         registrar.addConfig(LINKED_REPEATER, true);
-
-        registrar.registerComponentProvider(AdvancedBreweryProvider.INSTANCE, TooltipPosition.BODY, BlockAdvancedBrewery.class);
-        registrar.registerBlockDataProvider(AdvancedBreweryProvider.INSTANCE, TileAdvancedBrewery.class);
-        registrar.addConfig(ADVANCED_BREWERY, true);
     }
 }
