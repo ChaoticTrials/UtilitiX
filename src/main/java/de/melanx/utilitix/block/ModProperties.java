59740c57a3b26d2d9790abb4fffc2d14a84644f7
Liz Fransen <elizabethlfransen@gmail.com>
2022-05-12 16:36:10 -0500

    Add tank capabilities to tile entity

--- src/main/java/de/melanx/utilitix/block/ModProperties.java
+++ src/main/java/de/melanx/utilitix/block/ModProperties.java
@@ -8,9 +8,15 @@ import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.RailShape;
 import net.minecraft.world.level.material.Fluid;
 
+import java.util.Collection;
+import java.util.Set;
+
 public class ModProperties {
 
-    public static final TagKey<Fluid> XP_FLUID_TAG = TagKey.create(Registry.FLUID_REGISTRY, new ResourceLocation("experience"));
+    public static final Set<TagKey<Fluid>> XP_FLUID_TAGS = Set.of(
+            TagKey.create(Registry.FLUID_REGISTRY, new ResourceLocation("forge", "experience")),
+            TagKey.create(Registry.FLUID_REGISTRY, new ResourceLocation("forge", "xpjuice"))
+    );
 
     public static final EnumProperty<RailShape> RAIL_SHAPE_FLAT = EnumProperty.create("shape", RailShape.class, (shape) -> !shape.isAscending());
     public static final EnumProperty<RailShape> RAIL_SHAPE_FLAT_STRAIGHT = EnumProperty.create("shape", RailShape.class, (shape) -> shape == RailShape.NORTH_SOUTH || shape == RailShape.EAST_WEST);
