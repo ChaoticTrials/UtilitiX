967744d956f3e0b4c471ee06734bf2666d8649f3
MelanX <info@melanx.de>
2024-09-07 13:40:46 +0200

    add shulker chest boats item tags
    closes #67

--- src/main/java/de/melanx/utilitix/data/ModTagProvider.java
+++ src/main/java/de/melanx/utilitix/data/ModTagProvider.java
@@ -31,6 +31,8 @@ public class ModTagProvider extends CommonTagsProviderBase {
         this.item(ModItemTags.BOTTLES).add(Items.GLASS_BOTTLE, ModItems.failedPotion);
         this.item(ModItemTags.RAIL_POWER_SOURCES).add(Items.REDSTONE_TORCH, ModBlocks.weakRedstoneTorch.asItem());
         this.item(Tags.Items.SHEARS).add(ModItems.diamondShears);
+        this.item(ItemTags.CHEST_BOATS).addTag(ModItemTags.SHULKER_CHEST_BOATS);
+        this.item(ModItemTags.SHULKER_CHEST_BOATS).add(ModItems.oakShulkerBoat, ModItems.spruceShulkerBoat, ModItems.birchShulkerBoat, ModItems.jungleShulkerBoat, ModItems.acaciaShulkerBoat, ModItems.darkOakShulkerBoat, ModItems.mangroveShulkerBoat);
 
         this.copyBlock(BlockTags.RAILS, ItemTags.RAILS);
         this.copyBlock(BlockTags.WALLS, ItemTags.WALLS);
--- src/main/java/de/melanx/utilitix/registration/ModItemTags.java
+++ src/main/java/de/melanx/utilitix/registration/ModItemTags.java
@@ -12,4 +12,5 @@ public class ModItemTags {
     public static final TagKey<Item> BOTTLES = ItemTags.create(new ResourceLocation(UtilitiX.getInstance().modid, "bottles"));
     public static final TagKey<Item> RAIL_POWER_SOURCES = ItemTags.create(new ResourceLocation(UtilitiX.getInstance().modid, "rail_power_sources"));
     public static final TagKey<Item> CURIOS_BACK = ItemTags.create(new ResourceLocation("curios", "back"));
+    public static final TagKey<Item> SHULKER_CHEST_BOATS = ItemTags.create(new ResourceLocation(UtilitiX.getInstance().modid, "shulker_chest_boats"));
 }
