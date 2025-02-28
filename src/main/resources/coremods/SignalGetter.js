4cdcd5c1b4ed18ea34e3571581e2bb5f0563d3c3
MelanX <info@melanx.de>
2025-02-28 18:51:56 +0100

    Fix waterlogging when sneaking

--- src/main/resources/coremods/SignalGetter.js
+++ src/main/resources/coremods/SignalGetter.js
@@ -1,42 +1,33 @@
-import {
-    AbstractInsnNode,
-    ASMAPI,
-    CoreMods,
-    InsnList,
-    JumpInsnNode,
-    LabelNode,
-    MethodInsnNode,
-    MethodNode,
-    Opcodes,
-    VarInsnNode
-} from "coremods";
-import {InsnNode} from "./coremods";
+function initializeCoreMod() {
+    var ASMAPI = Java.type('net.minecraftforge.coremod.api.ASMAPI');
+    var Opcodes = Java.type('org.objectweb.asm.Opcodes');
+    var VarInsnNode = Java.type('org.objectweb.asm.tree.VarInsnNode');
+    var InsnNode = Java.type('org.objectweb.asm.tree.InsnNode');
+    var InsnList = Java.type('org.objectweb.asm.tree.InsnList');
 
-function initializeCoreMod(): CoreMods {
     return {
-        'NoWaterlog': {
+        'SignalGetter': {
             'target': {
                 'type': 'METHOD',
                 'class': 'net.minecraft.world.level.SignalGetter',
                 'methodName': 'm_277086_',
                 'methodDesc': '(Lnet/minecraft/core/BlockPos;)I'
             },
-            'transformer': function (method: MethodNode) {
+            'transformer': function (method) {
                 method.instructions.clear();
-
-                const target = new InsnList();
+                var target = new InsnList();
                 target.add(new VarInsnNode(Opcodes.ALOAD, 0));
                 target.add(new VarInsnNode(Opcodes.ALOAD, 1));
                 target.add(ASMAPI.buildMethodCall(
                     'de/melanx/utilitix/util/CoreUtil',
-                    'getBestNeighborSignalEdit', '(Lnet/minecraft/world/level/SignalGetter;Lnet/minecraft/core/BlockPos;)I',
-                    ASMAPI.MethodType.STATIC
-                ));
+                    'getBestNeighborSignalEdit',
+                    '(Lnet/minecraft/world/level/SignalGetter;Lnet/minecraft/core/BlockPos;)I',
+                    ASMAPI.MethodType.STATIC)
+                );
                 target.add(new InsnNode(Opcodes.IRETURN));
-
                 method.instructions.add(target);
                 return method;
             }
         }
-    }
+    };
 }
